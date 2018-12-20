ffb6b4466d97daf8c1dd4d01cedcb8880ff350d6
itsubaki <itsubaki@users.noreply.github.com>
2018-12-20 14:59:23 +0900

    Update costexp

--- internal/costexp/costexp_test.go
+++ internal/costexp/costexp_test.go
@@ -12,193 +12,248 @@ import (
 )
 
 func TestCostExplorer(t *testing.T) {
-	os.Setenv("AWS_PROFILE", "example")
+	os.Setenv("AWS_PROFILE", "aws")
 	c := costexplorer.New(session.Must(session.NewSession()))
 
 	period := &costexplorer.DateInterval{
-		Start: aws.String("2018-01-01"),
+		Start: aws.String("2018-11-01"),
 		End:   aws.String("2018-11-30"),
 	}
 
-	input := costexplorer.GetDimensionValuesInput{
-		Dimension:  aws.String("USAGE_TYPE"),
-		TimePeriod: period,
+	linked := []string{}
+	{
+		input := costexplorer.GetDimensionValuesInput{
+			Dimension:  aws.String("LINKED_ACCOUNT"),
+			TimePeriod: period,
+		}
+
+		out, err := c.GetDimensionValues(&input)
+		if err != nil {
+			t.Error(err)
+		}
+
+		for _, v := range out.DimensionValues {
+			linked = append(linked, *v.Value)
+		}
+
+		for _, v := range out.DimensionValues {
+			fmt.Printf("%v %v\n", *v.Value, *v.Attributes["description"])
+		}
+	}
+
+	usageTypeUnique := make(map[string]bool)
+	for i := 0; i < 3; i++ {
+		and := []*costexplorer.Expression{}
+		and = append(and, &costexplorer.Expression{
+			Dimensions: &costexplorer.DimensionValues{
+				Key:    aws.String("LINKED_ACCOUNT"),
+				Values: []*string{aws.String(linked[i])},
+			},
+		})
+
+		input := costexplorer.GetDimensionValuesInput{
+			Dimension:  aws.String("USAGE_TYPE"),
+			TimePeriod: period,
+		}
+
+		{
+			out, err := c.GetDimensionValues(&input)
+			if err != nil {
+				t.Errorf("%v", err)
+			}
+
+			filter := []string{
+				"BoxUsage",
+				"NodeUsage",
+				"InstanceUsage",
+				"Multi-AZUsage",
+			}
+
+			for _, d := range out.DimensionValues {
+				for _, f := range filter {
+					if strings.Contains(*d.Value, f) {
+						usageTypeUnique[*d.Value] = true
+					}
+				}
+			}
+		}
 	}
 
 	usageType := []string{}
-	{
-		out, err := c.GetDimensionValues(&input)
-		if err != nil {
-			t.Errorf("%v", err)
-		}
-
-		filter := []string{
-			"BoxUsage",
-			"NodeUsage",
-			"InstanceUsage",
-			"Multi-AZUsage",
-		}
-
-		for _, d := range out.DimensionValues {
-			for _, f := range filter {
-				if strings.Contains(*d.Value, f) {
-					usageType = append(usageType, *d.Value)
-				}
-			}
-		}
-
-		fmt.Println(usageType)
+	for k := range usageTypeUnique {
+		usageType = append(usageType, k)
 	}
 
-	{
-		or := []*costexplorer.Expression{}
-		for i := range usageType {
-			if !strings.Contains(usageType[i], "BoxUsage") {
-				continue
-			}
+	for i := 0; i < 3; i++ {
+		fmt.Println(linked[i])
 
-			or = append(or, &costexplorer.Expression{
-				Dimensions: &costexplorer.DimensionValues{
-					Key:    aws.String("USAGE_TYPE"),
-					Values: []*string{aws.String(usageType[i])},
-				},
-			})
-		}
-
-		input := costexplorer.GetCostAndUsageInput{
-			Filter:      &costexplorer.Expression{Or: or},
-			Metrics:     []*string{aws.String("UsageQuantity")},
-			Granularity: aws.String("MONTHLY"),
-			GroupBy: []*costexplorer.GroupDefinition{
-				{
-					Key:  aws.String("USAGE_TYPE"),
-					Type: aws.String("DIMENSION"),
-				},
-				{
-					Key:  aws.String("PLATFORM"),
-					Type: aws.String("DIMENSION"),
-				},
+		and := []*costexplorer.Expression{}
+		and = append(and, &costexplorer.Expression{
+			Dimensions: &costexplorer.DimensionValues{
+				Key:    aws.String("LINKED_ACCOUNT"),
+				Values: []*string{aws.String(linked[i])},
 			},
-			TimePeriod: period,
-		}
+		})
 
-		out, err := c.GetCostAndUsage(&input)
-		if err != nil {
-			t.Error(err)
-		}
-
-		for _, r := range out.ResultsByTime {
-			start := *r.TimePeriod.Start
-			end := *r.TimePeriod.End
-			for _, g := range r.Groups {
-				amount := *g.Metrics["UsageQuantity"].Amount
-				if amount == "0" {
+		// ec2
+		{
+			or := []*costexplorer.Expression{}
+			for i := range usageType {
+				if !strings.Contains(usageType[i], "BoxUsage") {
 					continue
 				}
 
-				fmt.Printf("date=%v~%v, usage_type=%v, platform=%v, instance_hrs=%v\n", start, end, *g.Keys[0], *g.Keys[1], amount)
-			}
-		}
-	}
-
-	{
-		or := []*costexplorer.Expression{}
-		for i := range usageType {
-			if !strings.Contains(usageType[i], "NodeUsage") {
-				continue
+				or = append(or, &costexplorer.Expression{
+					Dimensions: &costexplorer.DimensionValues{
+						Key:    aws.String("USAGE_TYPE"),
+						Values: []*string{aws.String(usageType[i])},
+					},
+				})
 			}
 
-			or = append(or, &costexplorer.Expression{
-				Dimensions: &costexplorer.DimensionValues{
-					Key:    aws.String("USAGE_TYPE"),
-					Values: []*string{aws.String(usageType[i])},
+			input := costexplorer.GetCostAndUsageInput{
+				Filter: &costexplorer.Expression{
+					And: append(and, &costexplorer.Expression{Or: or}),
 				},
-			})
+				Metrics:     []*string{aws.String("UsageQuantity")},
+				Granularity: aws.String("MONTHLY"),
+				GroupBy: []*costexplorer.GroupDefinition{
+					{
+						Key:  aws.String("USAGE_TYPE"),
+						Type: aws.String("DIMENSION"),
+					},
+					{
+						Key:  aws.String("PLATFORM"),
+						Type: aws.String("DIMENSION"),
+					},
+				},
+				TimePeriod: period,
+			}
+
+			out, err := c.GetCostAndUsage(&input)
+			if err != nil {
+				t.Error(err)
+			}
+
+			for _, r := range out.ResultsByTime {
+				start := *r.TimePeriod.Start
+				end := *r.TimePeriod.End
+				for _, g := range r.Groups {
+					amount := *g.Metrics["UsageQuantity"].Amount
+					if amount == "0" {
+						continue
+					}
+
+					fmt.Printf("date=%v~%v, usage_type=%v, platform=%v, instance_hrs=%v\n", start, end, *g.Keys[0], *g.Keys[1], amount)
+				}
+			}
 		}
 
-		input := costexplorer.GetCostAndUsageInput{
-			Filter:      &costexplorer.Expression{Or: or},
-			Metrics:     []*string{aws.String("UsageQuantity")},
-			Granularity: aws.String("MONTHLY"),
-			GroupBy: []*costexplorer.GroupDefinition{
-				{
-					Key:  aws.String("USAGE_TYPE"),
-					Type: aws.String("DIMENSION"),
-				},
-				{
-					Key:  aws.String("CACHE_ENGINE"),
-					Type: aws.String("DIMENSION"),
-				},
-			},
-			TimePeriod: period,
-		}
-
-		out, err := c.GetCostAndUsage(&input)
-		if err != nil {
-			t.Error(err)
-		}
-
-		for _, r := range out.ResultsByTime {
-			start := *r.TimePeriod.Start
-			end := *r.TimePeriod.End
-			for _, g := range r.Groups {
-				amount := *g.Metrics["UsageQuantity"].Amount
-				if amount == "0" {
+		// elasticache
+		{
+			or := []*costexplorer.Expression{}
+			for i := range usageType {
+				if !strings.Contains(usageType[i], "NodeUsage") {
 					continue
 				}
 
-				fmt.Printf("date=%v~%v, usage_type=%v, engine=%v, instance_hrs=%v\n", start, end, *g.Keys[0], *g.Keys[1], amount)
-			}
-		}
-	}
-
-	{
-		or := []*costexplorer.Expression{}
-		for i := range usageType {
-			if !strings.Contains(usageType[i], "InstanceUsage") && !strings.Contains(usageType[i], "Multi-AZUsage") {
-				continue
+				or = append(or, &costexplorer.Expression{
+					Dimensions: &costexplorer.DimensionValues{
+						Key:    aws.String("USAGE_TYPE"),
+						Values: []*string{aws.String(usageType[i])},
+					},
+				})
 			}
 
-			or = append(or, &costexplorer.Expression{
-				Dimensions: &costexplorer.DimensionValues{
-					Key:    aws.String("USAGE_TYPE"),
-					Values: []*string{aws.String(usageType[i])},
+			input := costexplorer.GetCostAndUsageInput{
+				Filter: &costexplorer.Expression{
+					And: append(and, &costexplorer.Expression{Or: or}),
 				},
-			})
+				Metrics:     []*string{aws.String("UsageQuantity")},
+				Granularity: aws.String("MONTHLY"),
+				GroupBy: []*costexplorer.GroupDefinition{
+					{
+						Key:  aws.String("USAGE_TYPE"),
+						Type: aws.String("DIMENSION"),
+					},
+					{
+						Key:  aws.String("CACHE_ENGINE"),
+						Type: aws.String("DIMENSION"),
+					},
+				},
+				TimePeriod: period,
+			}
+
+			out, err := c.GetCostAndUsage(&input)
+			if err != nil {
+				t.Error(err)
+			}
+
+			for _, r := range out.ResultsByTime {
+				start := *r.TimePeriod.Start
+				end := *r.TimePeriod.End
+				for _, g := range r.Groups {
+					amount := *g.Metrics["UsageQuantity"].Amount
+					if amount == "0" {
+						continue
+					}
+
+					fmt.Printf("date=%v~%v, usage_type=%v, engine=%v, instance_hrs=%v\n", start, end, *g.Keys[0], *g.Keys[1], amount)
+				}
+			}
 		}
 
-		input := costexplorer.GetCostAndUsageInput{
-			Filter:      &costexplorer.Expression{Or: or},
-			Metrics:     []*string{aws.String("UsageQuantity")},
-			Granularity: aws.String("MONTHLY"),
-			GroupBy: []*costexplorer.GroupDefinition{
-				{
-					Key:  aws.String("USAGE_TYPE"),
-					Type: aws.String("DIMENSION"),
-				},
-				{
-					Key:  aws.String("DATABASE_ENGINE"),
-					Type: aws.String("DIMENSION"),
-				},
-			},
-			TimePeriod: period,
-		}
-
-		out, err := c.GetCostAndUsage(&input)
-		if err != nil {
-			t.Error(err)
-		}
-
-		for _, r := range out.ResultsByTime {
-			start := *r.TimePeriod.Start
-			end := *r.TimePeriod.End
-			for _, g := range r.Groups {
-				amount := *g.Metrics["UsageQuantity"].Amount
-				if amount == "0" {
+		// rds
+		{
+			or := []*costexplorer.Expression{}
+			for i := range usageType {
+				if !strings.Contains(usageType[i], "InstanceUsage") && !strings.Contains(usageType[i], "Multi-AZUsage") {
 					continue
 				}
 
-				fmt.Printf("date=%v~%v, usage_type=%v, engine=%v, instance_hrs=%v\n", start, end, *g.Keys[0], *g.Keys[1], amount)
+				or = append(or, &costexplorer.Expression{
+					Dimensions: &costexplorer.DimensionValues{
+						Key:    aws.String("USAGE_TYPE"),
+						Values: []*string{aws.String(usageType[i])},
+					},
+				})
+			}
+
+			input := costexplorer.GetCostAndUsageInput{
+				Filter: &costexplorer.Expression{
+					And: append(and, &costexplorer.Expression{Or: or}),
+				},
+				Metrics:     []*string{aws.String("UsageQuantity")},
+				Granularity: aws.String("MONTHLY"),
+				GroupBy: []*costexplorer.GroupDefinition{
+					{
+						Key:  aws.String("USAGE_TYPE"),
+						Type: aws.String("DIMENSION"),
+					},
+					{
+						Key:  aws.String("DATABASE_ENGINE"),
+						Type: aws.String("DIMENSION"),
+					},
+				},
+				TimePeriod: period,
+			}
+
+			out, err := c.GetCostAndUsage(&input)
+			if err != nil {
+				t.Error(err)
+			}
+
+			for _, r := range out.ResultsByTime {
+				start := *r.TimePeriod.Start
+				end := *r.TimePeriod.End
+				for _, g := range r.Groups {
+					amount := *g.Metrics["UsageQuantity"].Amount
+					if amount == "0" {
+						continue
+					}
+
+					fmt.Printf("date=%v~%v, usage_type=%v, engine=%v, instance_hrs=%v\n", start, end, *g.Keys[0], *g.Keys[1], amount)
+				}
 			}
 		}
 	}
