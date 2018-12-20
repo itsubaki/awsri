1ad0e98fa90f6315790713646ac5fa2b80ffe356
itsubaki <itsubaki@users.noreply.github.com>
2018-12-20 16:42:08 +0900

    Update costexp

--- internal/costexp/costexp.go
+++ internal/costexp/costexp.go
@@ -1,6 +1,17 @@
 package costexp
 
-type OutputCostExp struct {
+import (
+	"fmt"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/costexplorer"
+)
+
+type LinkedAccount struct {
+	AccountID   string
+	Description string
 }
 
 type UsageQuantityList []*UsageQuantity
@@ -13,4 +24,78 @@ type UsageQuantity struct {
 	Engine          string  `json:"engine,omitempty"`
 	InstanceHour    float64 `json:"instance_hour"`
 	InstanceNum     float64 `json:"instance_num"`
+}
+
+type CostExp struct {
+	Client *costexplorer.CostExplorer
+}
+
+func New() *CostExp {
+	return &CostExp{
+		Client: costexplorer.New(session.Must(session.NewSession())),
+	}
+}
+
+func (c *CostExp) GetUsageQuantity() (UsageQuantityList, error) {
+	out := UsageQuantityList{}
+
+	return out, nil
+}
+
+func (c *CostExp) GetUsageType(period *costexplorer.DateInterval) ([]string, error) {
+	usageTypeUnique := make(map[string]bool)
+	input := costexplorer.GetDimensionValuesInput{
+		Dimension:  aws.String("USAGE_TYPE"),
+		TimePeriod: period,
+	}
+
+	val, err := c.Client.GetDimensionValues(&input)
+	if err != nil {
+		return []string{}, fmt.Errorf("get dimenstion value: %v", err)
+	}
+
+	filter := []string{
+		"BoxUsage",
+		"NodeUsage",
+		"InstanceUsage",
+		"Multi-AZUsage",
+	}
+
+	for _, d := range val.DimensionValues {
+		for _, f := range filter {
+			if strings.Contains(*d.Value, f) {
+				usageTypeUnique[*d.Value] = true
+			}
+		}
+	}
+
+	out := []string{}
+	for u := range usageTypeUnique {
+		out = append(out, u)
+	}
+
+	return out, nil
+}
+
+func (c *CostExp) GetLinkedAccount(period *costexplorer.DateInterval) ([]LinkedAccount, error) {
+	out := []LinkedAccount{}
+
+	input := costexplorer.GetDimensionValuesInput{
+		Dimension:  aws.String("LINKED_ACCOUNT"),
+		TimePeriod: period,
+	}
+
+	val, err := c.Client.GetDimensionValues(&input)
+	if err != nil {
+		return out, fmt.Errorf("get dimension value: %v", err)
+	}
+
+	for _, v := range val.DimensionValues {
+		out = append(out, LinkedAccount{
+			AccountID:   *v.Value,
+			Description: *v.Attributes["description"],
+		})
+	}
+
+	return out, nil
 }
--- internal/costexp/costexp_test.go
+++ internal/costexp/costexp_test.go
@@ -11,6 +11,42 @@ import (
 	"github.com/aws/aws-sdk-go/service/costexplorer"
 )
 
+func TestLinkedAccount(t *testing.T) {
+	os.Setenv("AWS_PROFILE", "aws")
+	period := &costexplorer.DateInterval{
+		Start: aws.String("2018-01-01"),
+		End:   aws.String("2018-11-30"),
+	}
+
+	c := New()
+	list, err := c.GetLinkedAccount(period)
+	if err != nil {
+		t.Errorf("get linked account: %v", err)
+	}
+
+	if len(list) < 1 {
+		t.Errorf("linked account is empty")
+	}
+}
+
+func TestUsageType(t *testing.T) {
+	os.Setenv("AWS_PROFILE", "aws")
+	period := &costexplorer.DateInterval{
+		Start: aws.String("2018-01-01"),
+		End:   aws.String("2018-11-30"),
+	}
+
+	c := New()
+	list, err := c.GetUsageType(period)
+	if err != nil {
+		t.Errorf("get usage type: %v", err)
+	}
+
+	if len(list) < 1 {
+		t.Errorf("usage type is empty")
+	}
+}
+
 func TestCostExplorer(t *testing.T) {
 	os.Setenv("AWS_PROFILE", "aws")
 	c := costexplorer.New(session.Must(session.NewSession()))
