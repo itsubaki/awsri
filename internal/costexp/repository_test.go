d1ad2d2697fb37ee86ba9eb46fe747fc738b5703
itsubaki <itsubaki@users.noreply.github.com>
2018-12-26 13:03:23 +0900

    Move TestCase

--- internal/costexp/repository_test.go
+++ internal/costexp/repository_test.go
@@ -6,6 +6,35 @@ import (
 	"testing"
 )
 
+func TestMergedRepository(t *testing.T) {
+	dir := fmt.Sprintf(
+		"%s/%s",
+		os.Getenv("GOPATH"),
+		"src/github.com/itsubaki/awsri/internal/_serialized/costexp",
+	)
+
+	path := []string{
+		fmt.Sprintf("%s/%s", dir, "example_2017-12.out"),
+		fmt.Sprintf("%s/%s", dir, "example_2018-01.out"),
+	}
+
+	repo := &Repository{
+		Profile: "example",
+	}
+
+	for _, p := range path {
+		p, err := NewRepository(p)
+		if err != nil {
+			t.Errorf("new costexp repository: %v", err)
+		}
+		repo.Internal = append(repo.Internal, p.Internal...)
+	}
+
+	if len(repo.SelectAll()) < 0 {
+		t.Errorf("invalid repository")
+	}
+}
+
 func TestRepository(t *testing.T) {
 	path := fmt.Sprintf(
 		"%s/%s/%s.out",
