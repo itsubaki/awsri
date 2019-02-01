4aa3f428ecdc68876f979c979278bd63abd2018d
itsubaki <itsubaki@users.noreply.github.com>
2019-02-01 19:01:13 +0900

    Add Platform to OperatingSystem

---
+++ pkg/awsprice/operatingsystem.go
@@ -0,0 +1,8 @@
+package awsprice
+
+// costexp/platform -> awsprice/operatingsystem
+var OperationgSystem = map[string]string{
+	"Amazon Linux":            "Linux",
+	"Linux/UNIX":              "Linux",
+	"Linux with SQL Standard": "Linux",
+}
