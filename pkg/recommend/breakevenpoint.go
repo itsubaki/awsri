3f0c4210d9fa117d4bf8d157463b769f6397363b
itsubaki <itsubaki@users.noreply.github.com>
2019-09-27 12:12:07 +0900

    Update recommend

--- pkg/recommend/breakevenpoint.go
+++ pkg/recommend/breakevenpoint.go
@@ -1,7 +1,23 @@
 package recommend
 
-import "github.com/itsubaki/hermes/pkg/usage"
+import (
+	"github.com/itsubaki/hermes/pkg/pricing"
+)
 
-func BreakEvenPoint(quantity usage.Quantity) int {
-	return -1
+func BreakEvenPoint(p pricing.Price) int {
+	month := 12
+	if p.LeaseContractLength == "3yr" {
+		month = 12 * 3
+	}
+
+	out, ond, res := 0, 0.0, p.ReservedQuantity
+	for i := 1; i < month+1; i++ {
+		ond, res = ond+p.OnDemand*24*float64(Days[i]), res+p.ReservedHrs*24*float64(Days[i])
+		if ond > res {
+			out = i
+			break
+		}
+	}
+
+	return out
 }
