be246262e6f791aed53521ad5a9a1cc1dd174835
itsubaki <itsubaki@users.noreply.github.com>
2020-03-19 10:22:57 +0900

    Add hermes.go

--- pkg/reservation/utilization_test.go
+++ pkg/reservation/utilization_test.go
@@ -9,7 +9,7 @@ import (
 func TestFetch(t *testing.T) {
 	os.Setenv("AWS_PROFILE", "example")
 
-	u, err := Fetch("2019-09-01", "2019-09-02")
+	u, err := Fetch("2020-02-01", "2020-02-02")
 	if err != nil {
 		t.Errorf("fetch: %v", err)
 	}
