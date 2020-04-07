8bfdc67f575a7fb7dc8f292060f03880cc6e0909
itsubaki <itsubaki@users.noreply.github.com>
2020-04-07 17:44:32 +0900

    Add sha256 to reservation.Utilization

--- pkg/reservation/utilization.go
+++ pkg/reservation/utilization.go
@@ -2,6 +2,8 @@ package reservation
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"sort"
@@ -30,6 +32,11 @@ type Utilization struct {
 	Num              float64 `json:"num"`
 	Percentage       float64 `json:"percentage"`
 	CoveringCost     float64 `json:"covering_cost"` // ondemand cost
+}
+
+func (u Utilization) Sha256() string {
+	sha := sha256.Sum256([]byte(u.JSON()))
+	return hex.EncodeToString(sha[:])
 }
 
 func (u Utilization) UsageType() string {
