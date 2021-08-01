98b3b56d09cd4988561b59de950426203db4d33d
itsubaki <1759459+itsubaki@users.noreply.github.com>
2021-08-01 17:23:39 +0900

    Update main.go

--- main.go
+++ main.go
@@ -40,9 +40,7 @@ func New(version string) *cli.App {
 		Usage:   "ap-east-1, ap-south-1, ap-northeast-1, ap-northeast-2, ap-northeast-3, ap-southeast-1, ap-southeast-2, eu-north-1, eu-west-1, eu-west-2, eu-west-3, eu-central-1, us-east-1, us-east-2, us-west-1, us-west-2, us-gov-east-1, us-gov-west-1, ca-central-1, sa-east-1, me-south-1",
 		Value: cli.NewStringSlice(
 			"ap-northeast-1",
-			"ap-southeast-1",
-			"us-west-1",
-			"us-west-2",
+			"us-east-1",
 		),
 	}
 
@@ -73,11 +71,7 @@ func New(version string) *cli.App {
 				EnvVars: []string{"METRICS"},
 				Usage:   "NetAmortizedCost, NetUnblendedCost, UnblendedCost, AmortizedCost, BlendedCost",
 				Value: cli.NewStringSlice(
-					"NetAmortizedCost",
-					"NetUnblendedCost",
 					"UnblendedCost",
-					"AmortizedCost",
-					"BlendedCost",
 				),
 			},
 		},
